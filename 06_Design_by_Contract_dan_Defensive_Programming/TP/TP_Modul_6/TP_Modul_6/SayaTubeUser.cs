47128bef30a700193553a718fe9a7ad4f9b2a1f8
zhafiravel <zhafiravel125@gmail.com>
2025-03-21 09:05:25 +0700

    add source code for TP

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/TP/TP_Modul_6/TP_Modul_6/SayaTubeUser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class SayaTubeUser
+{
+    private int id;
+    private string username;
+    private List<SayaTubeVideo> uploadedVideos;
+
+    public SayaTubeUser(string username)
+    {
+        Debug.Assert(username != null && username.Length <= 100, "Username harus tidak null dan maksimal 100 karakter");
+
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999); // Generate ID random 5 digit
+        this.username = username;
+        this.uploadedVideos = new List<SayaTubeVideo>();
+    }
+
+    public void AddVideo(SayaTubeVideo video)
+    {
+        Debug.Assert(video != null, "Video yang ditambahkan tidak boleh null");
+
+        uploadedVideos.Add(video);
+    }
+
+    public int GetTotalVideoPlayCount()
+    {
+        int totalPlayCount = 0;
+        foreach (var video in uploadedVideos)
+        {
+            totalPlayCount += video.GetPlayCount();
+        }
+        return totalPlayCount;
+    }
+
+    public void PrintAllVideoPlaycount()
+    {
+        Console.WriteLine($"User: {username}");
+
+        int maxPrint = Math.Min(8, uploadedVideos.Count); // Maksimal print 8 video
+        for (int i = 0; i < maxPrint; i++)
+        {
+            Console.WriteLine($"Video {i + 1} judul: {uploadedVideos[i].GetTitle()}");
+        }
+    }
+}
---
+++ 06_Design_by_Contract_dan_Defensive_Programming/TP/TP_Modul_6/TP_Modul_6/SayaTubeVideo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Debug.Assert(title != null && title.Length <= 200, "Judul video harus tidak null dan maksimal 200 karakter");
+
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999); // Generate ID random 5 digit
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        if (count <= 0 || count > 25000000)
+        {
+            Console.WriteLine("ERROR: Play count harus antara 1 hingga 25 juta!");
+            return;
+        }
+
+        try
+        {
+            checked
+            {
+                playCount += count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("ERROR: Play count melebihi batas integer maksimum!");
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"Video ID: {id}");
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
+    }
+
+    public int GetPlayCount()
+    {
+        return playCount;
+    }
+
+    public string GetTitle()
+    {
+        return title;
+    }
+}
